f76e766dd2ecd7dbba32f9287b21603afc5d1c31
brittney betzold <brittney.betzold2022@gmail.com>
2024-08-12 22:22:31 -0500

    googleAPI

--- src/controllers/search.js
+++ src/controllers/search.js
@@ -1,7 +1,24 @@
-const { StatusCodes } = require('http-status-codes') 
+// controllers/search.js
+const { getCoordinates, getPlacesNearby } = require("../utils/googleMaps");
+const { StatusCodes } = require("http-status-codes");
 
 const searchEngine = async (req, res) => {
+  const { address, radius = 500, type = "restaurant" } = req.query;
 
-}
+  try {
+    // First, get the coordinates of the provided address
+    const location = await getCoordinates(address);
 
-module.exports = { searchEngine }
+    // Then, search for places nearby those coordinates
+    const places = await getPlacesNearby(location, radius, type);
+
+    res.status(StatusCodes.OK).json({ places });
+  } catch (error) {
+    console.error("Error searching for places:", error);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ msg: "Failed to fetch nearby places" });
+  }
+};
+
+module.exports = { searchEngine };
