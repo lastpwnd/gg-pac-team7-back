40774176345a48a65f0e23e5dc6dfe832d3fabac
lastpwnd <last.pwnd@gmail.com>
2024-09-04 21:26:22 -0400

    almost-final

--- src/controllers/profile.js
+++ src/controllers/profile.js
@@ -1,14 +1,15 @@
 const User = require('../models/User')
 const Bookmarks = require('../models/Bookmark')
 const { StatusCodes } = require('http-status-codes') 
+const { avatarsList } = require('../../utils/avatarsList')
 
 const getUserProfile = async (req, res) => { // Returns profile data of logged-in user
     try {
-        const user = await User.findOne({ _id: req.user.userId}).select('firstName lastName userName avatarUrl -_id')
+        const user = await User.findOne({ _id: req.user.userId}).select('firstName lastName userName avatarUrl')
         const bookmarks = await Bookmarks.find({user: req.user.userId}).select('event -_id').populate('event')
         res.status(StatusCodes.OK).json({...user._doc, bookmarks})        
     } catch (error) {
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: "Failed to load your profile data" })  
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json([{ msg: "Failed to load your profile data" }])  
     }
 }
 
@@ -16,9 +17,18 @@ const getOtherUserProfile = async (req, res) => { // Returns designated user's p
     try {
         const otherUser = await User.findOne({userName: req.params.username})
         const bookmarks = await Bookmarks.find({user: otherUser._id}).select('event -_id').populate('event')
-        // if param refers to logged-in user, redirects and calls getUserProfile controller
-        if (req.user.userId == otherUser._id) {
-            return res.status(StatusCodes.MOVED_TEMPORARILY).redirect('/api/v1/profile') 
+        // if param refers to logged-in user, 302 status generated (treat as error)  
+        if(!otherUser){
+            return res.status(StatusCodes.BAD_REQUEST).json([{
+                type: "params",
+                value:`${req.body.userName}`,
+                msg: 'Wrong credentials provided, has to be "@username"',
+                path: "path", 
+                location: 'req.params', 
+            }])
+        }
+        else if (req.user.userId == otherUser._id) {
+            return res.status(StatusCodes.MOVED_TEMPORARILY).json([{location: '/profile', msg: "Redirect required"}]) 
         }
         else
         {   
@@ -26,17 +36,17 @@ const getOtherUserProfile = async (req, res) => { // Returns designated user's p
             res.status(StatusCodes.OK).json({...user, bookmarks}) 
         }   
     } catch (error) {
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: "Failed to load user's data" })
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json([{ msg: "Failed to load user's data" }])
     }
 }
 
 
 const editUserProfile = async (req, res) => { // sends data for profiles editing form
     try {
-        const user = await User.findOne({ _id: req.user.userId}).select('-password')
-        res.status(StatusCodes.OK).json(user)
+        const user = await User.findOne({ _id: req.user.userId}).select('firstName lastName userName avatarUrl')
+        res.status(StatusCodes.OK).json({...user._doc, avatarsList})
     } catch (error) {
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: "Failed to load data" })
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json([{ msg: "Failed to load profile data" }])
     }
 }
 
@@ -52,20 +62,35 @@ const updateUserProfile = async (req, res) => { // updates profile data in DB
                     userName: req.body.userName,
                     avatarUrl: req.body.avatarUrl
                 }).select('-password')     
-                return res.status(StatusCodes.OK).json({ msg: "Profile is successfully updated!" })
+                return res.status(StatusCodes.OK).json([{ msg: "Your profile was successfully updated!" }])
             } catch (error) {
-                let errorMsg = "Failed to update profile"
-                if (error.code == 11000) {
-                    errorMsg = "This nickname is already registered!"
+                if (error.status == 500)
+                {
+                    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json([{ msg: "Failed updating profile" }])
                 }
-                return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: errorMsg })
+                else if (error.code == 11000) {
+                    return res.status(StatusCodes.CONFLICT).json([{         
+                        type: "field",
+                        value:`${req.body.userName}`, 
+                        msg: "This username is already taken",
+                        path: "userName",
+                        location: "body"
+                    }])
+                }
+
             } 
         }   
         else {
-            return res.status(StatusCodes.BAD_REQUEST).json({ msg: "Incorrect password!" }) 
+            return res.status(StatusCodes.UNAUTHORIZED).json([{ 
+                type: "field",
+                value:`${req.body.password}`, // <- keep it?
+                msg: "Incorrect password!",
+                path: "password",
+                location: "body" 
+            }]) 
         }    
     } catch (error) {
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: "Failed updating profile" })
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json([{ msg: "Failed updating profile" }])
     }
 }
 
